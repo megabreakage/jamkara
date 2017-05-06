ea856e8ae435b3d352aeae91ab3ea5cec98cfe53
megabreakage <martin.njau@live.com>
2017-05-06 22:52:08 +0300

    too many changes I did not keep track :-(

--- js/js.js
+++ js/js.js
@@ -1,27 +1,12 @@
 // business logic
-  var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-      return true;
-    } else {
-      return false;
+
+
+// user interface logic
+$(document).ready(function() {
+  $("#aboutButton1").click(function() {
+    function myFunction(){
+      $("#mission").show(5000);
+      $("#vision").show(5000);
     }
-  };
-
-  // user interface logic
-  $(document).ready(function() {
-    $("form#leap-year").submit(function(event) {
-      event.preventDefault();
-      var year = parseInt($("input#year").val());
-      var result = leapYear(year);
-
-      $(".year").text(year);
-
-      if (!result) {                 // same as writing if (result === false)
-        $(".not").text("not");
-      } else {
-        $(".not").text("");
-      }
-
-      $("#result").show();
-    });
   });
+});
