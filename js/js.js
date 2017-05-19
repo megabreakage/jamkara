a950fb53f96de9d07a744ebb6203da9965107f19
megabreakage <martin.njau@live.com>
2017-05-19 17:55:14 +0300

    tweaks code to facilitate smooth load on viewport

--- js/js.js
+++ js/js.js
@@ -5,11 +5,17 @@
 $(document).ready(function() {
 
   //add animated class to p
-  $('p').addClass('animated fadeIn');
+  // $('p').addClass('animated fadeIn');
   $('h1','h2','h3','h4','h5','h6').addClass('animated bounceInLeft');
 
   //corousel code
   $('#carouselFade').carousel();
+
+  // for loading elements on viewport
+  new WOW().init();
+
+  // $('h1, h2, h3, h4, h5, h6').addClass('animated wow bounceInLeft');
+  // $('p').addClass('animated wow fadeIn');
 
   //START THE CODE FOR SMOOTH SCROLLING
 
@@ -35,16 +41,7 @@ $('a[href*="#"]')
         $('html, body').animate({
           scrollTop: target.offset().top
         }, 2000, function() {
-          // Callback after animation
-          // Must change focus!
-          // var $target = $(target);
-          // $target.focus();
-          // if ($target.is(":focus")) { // Checking if the target was focused
-          //   return false;
-          // } else {
-          //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          //   $target.focus(); // Set focus again
-          // };
+
         });
       }
     }
