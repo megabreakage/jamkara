8b5fee7832e7780122c0681d8eeac3d470a484f3
megabreakage <martin.njau@live.com>
2017-06-14 16:13:02 +0300

    adds a php file for the contact form

--- contactform.php
+++ contactform.php
@@ -2,10 +2,18 @@
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) ){
   $name = $_POST['name'];
   $email = $_POST['email'];
-  $message = $_POST['message'];
+  $message = nl2br($_POST['message']);
   $to = "martin.njau@live.com";
   $from = $email;
   $subject = 'Jamkara Contact Form Msg';
-  $comments = '<b'
+  $comments = <p> '.$message.' </p>;
+  $headers = "From: $from\n";
+  $headers .= "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8856-1\n";
+  if( mail($to, $subject, $comments, $headers) ){
+    echo "success";
+  } else {
+    echo "Server failed to send the message, Please try again later!"
+  }
 }
  ?>
